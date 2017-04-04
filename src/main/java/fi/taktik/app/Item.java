6dd1b6d78f8d0621aa2ba4287d1b18f4b57b50cc
Sami Kotkavuori <sami.kotkavuori@gmail.com>
2017-04-04 17:40:53 +0300

    Add item class and variables

---
+++ src/main/java/fi/taktik/app/Item.java
@@ -0,0 +1,17 @@
+package fi.taktik.app;
+
+
+public class Item {
+
+    private int id;
+    private String genre;
+    private String name;
+    private String description;
+    private double price;
+    private double discountPrice;
+    private String publisher;
+    private String developer;
+    private String[] platforms;
+
+    
+}
