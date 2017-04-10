ec13c107d140af45fb97f05f36513e0755da9e9b
Mickyei <micky.kyei@cs.tamk.fi>
2017-04-10 19:10:48 +0300

    Add functionality to ItemRepository

--- src/main/java/fi/taktik/app/ItemRepository.java
+++ src/main/java/fi/taktik/app/ItemRepository.java
@@ -3,4 +3,10 @@ package fi.taktik.app;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<Item,Integer>{
+
+    public Iterable<Item> findAll();
+    public void delete(Item entity);
+    public void delete(Long id);
+    public Workout findOne(Long id);
+
 }
