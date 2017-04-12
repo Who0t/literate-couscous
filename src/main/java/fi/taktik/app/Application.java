ff81f7dd9bd5da3c358eb1fc48af7fb943dfad06
Sami Kotkavuori <sami.kotkavuori@gmail.com>
2017-04-12 21:59:48 +0300

    Add useable URL print on startup

--- src/main/java/fi/taktik/app/Application.java
+++ src/main/java/fi/taktik/app/Application.java
@@ -7,5 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("GET: http://localhost:8080/items");
+        System.out.println("GET: http://localhost:8080/items/{id}");
+        System.out.println("POST: http://localhost:8080/items");
     }
 }
